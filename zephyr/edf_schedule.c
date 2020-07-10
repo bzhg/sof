257c85334c84b6594d308fd5e9e3c712e2fc8a0d
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2020-07-10 14:55:09 +0100

    zephyr: wrapper: edf_scheduler - set low priority and cleanup - SQUASH
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- zephyr/edf_schedule.c
+++ zephyr/edf_schedule.c
@@ -16,6 +16,12 @@
 struct k_work_q edf_workq;
 K_THREAD_STACK_DEFINE(edf_workq_stack, 8192);
 
+/*
+ * since only IPC is using the EDF scheduler - we schedule the work in the
+ * next timer_domain time slice
+ */
+#define EDF_SCHEDULE_DELAY	0
+
 static void edf_work_handler(struct k_work *work)
 {
 	struct task *task = CONTAINER_OF(work, struct task, z_delayed_work);
@@ -31,7 +37,8 @@ static void edf_work_handler(struct k_work *work)
 static int schedule_edf_task(void *data, struct task *task, uint64_t start,
 			      uint64_t period)
 {
-	k_timeout_t start_time = K_USEC(start + 1000);
+	/* start time is microseconds from now */
+	k_timeout_t start_time = K_USEC(start + EDF_SCHEDULE_DELAY);
 
 	k_delayed_work_submit_to_queue(&edf_workq,
 				       &task->z_delayed_work,
@@ -61,6 +68,12 @@ static int schedule_edf_task_complete(void *data, struct task *task)
 	return 0;
 }
 
+static int schedule_edf_task_running(void *data, struct task *task)
+{
+	task->state = SOF_TASK_STATE_RUNNING;
+	return 0;
+}
+
 static int schedule_edf_task_free(void *data, struct task *task)
 {
 	task->state = SOF_TASK_STATE_FREE;
@@ -72,13 +85,10 @@ static int schedule_edf_task_free(void *data, struct task *task)
 
 struct scheduler_ops schedule_edf_ops = {
 	.schedule_task		= schedule_edf_task,
-//	.schedule_task_running	= schedule_edf_task_running,
+	.schedule_task_running	= schedule_edf_task_running,
 	.schedule_task_complete = schedule_edf_task_complete,
-	.reschedule_task	= NULL,
 	.schedule_task_cancel	= schedule_edf_task_cancel,
 	.schedule_task_free	= schedule_edf_task_free,
-//	.scheduler_free		= scheduler_free_edf,
-//	.scheduler_run		= schedule_edf
 };
 
 int scheduler_init_edf(void)
@@ -88,7 +98,7 @@ int scheduler_init_edf(void)
 	k_work_q_start(&edf_workq,
 		       edf_workq_stack,
 		       K_THREAD_STACK_SIZEOF(edf_workq_stack),
-		       -1);
+		       1);
 	k_thread_name_set(&edf_workq.thread, "edf_workq");
 
 	return 0;
